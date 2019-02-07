a7fee99f23581db4d93e50e832cb68e1b38a3433
Nuno Alves de Sousa <nuno@uvic.ca>
2019-02-07 19:14:22 +0000

    Added dgemm benchmark

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -20,11 +20,22 @@ void printMatrix(const char* str, Matrix matrix)
 
 int main()
 {
+//    using CMatrixLib::T;
+
+    double alpha = 1.0;
+    double beta = 2.0;
+
     constexpr int dim = 5;
     CMatrixLib::FMatrix mA(dim, dim, 1.0, false);
-    CMatrixLib::FMatrix mB(dim, dim, 2, true);
+    CMatrixLib::FMatrix mB(dim, dim, 2, false);
+    CMatrixLib::FMatrix mC(dim, dim, 3, false);
 
     PRINT_MATRIX(mA);
     PRINT_MATRIX(mB);
-    PRINT_MATRIX(mA*mB);
+    PRINT_MATRIX(mC);
+
+    // Useful for benchmarking
+    mC = alpha * T(mA) * mB + beta * mC;
+
+    PRINT_MATRIX(mC);
 }
